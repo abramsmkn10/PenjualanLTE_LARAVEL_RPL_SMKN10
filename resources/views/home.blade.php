c3e7fca9dea093b834e8ada961c6bf2cbb85d2df
abramsmkn10 <abdurcybersoft@gmail.com>
2019-04-17 19:10:19 +0700

    Abdurrahman Penjualan LTE

---
+++ resources/views/home.blade.php
@@ -0,0 +1,33 @@
+@extends('admin.layout.master')
+
+@section('status7')
+class="treeview"
+@endsection
+
+@section('status10')
+class="treeview"
+@endsection
+
+@section('status11')
+class="treeview active"
+@endsection
+
+@section('status9')
+class="treeview"
+@endsection
+
+
+@section('nama')
+Dashboard
+@endsection
+
+@section('nama3')
+Control panel
+@endsection
+
+@section('nama1')
+Dashboard
+@endsection
+
+@section('content')
+@endsection
